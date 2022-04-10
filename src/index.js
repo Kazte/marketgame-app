4c6c0a576c8514239300476e2e8b1c93b447400a
Franco Castellitto <castellittofranco1@gmail.com>
2022-04-10 13:44:18 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1- Tener la variable react en scope
+import React from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 2- Tener la variable reactDOM en scope
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3- tener la variable App en scope
+function App() {
+    return <h1>Hola mundo</h1>;
+}
+
+// 4- Hacer render de la app
+ReactDOM.render(<App />, document.getElementById("root"));
