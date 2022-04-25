7758d1f1fd5304dd54aed6856cd6f848dea3aa4e
Franco Castellitto <castellittofranco1@gmail.com>
2022-04-25 19:36:22 -0300

    Loader Spinner

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div className="loader__spinner"></div>
+            <p>loading...</p>
+        </div>
+    );
+};
+
+export default Loader;
